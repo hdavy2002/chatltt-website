0e832230eb129635a322c6a534eb0d127fa93576
ChatLTT <hdavy2002@gmail.com>
2024-07-15 17:41:44 +0530

    Staatic deployment 6c4425eb6cb6

--- redirect.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href^="/listing"]');
-    links.forEach(function(link) {
-        link.href = link.href.replace("/listing", "https://app.chatltt.com/listing");
-    });
-});
