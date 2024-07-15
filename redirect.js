49da1e2b6261d29e7a1f1286657cd015a140357b
ChatLTT <hdavy2002@gmail.com>
2024-07-15 17:26:31 +0530

    Staatic deployment 07e13fd47b08

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href*="/listing"]');
+    var links = document.querySelectorAll('a[href^="/listing"]');
     links.forEach(function(link) {
         link.href = link.href.replace("/listing", "https://app.chatltt.com/listing");
     });
