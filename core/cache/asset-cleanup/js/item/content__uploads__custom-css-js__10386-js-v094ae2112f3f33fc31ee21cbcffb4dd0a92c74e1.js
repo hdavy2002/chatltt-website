5702fbd32a00331dc2b518429a211446b1c52570
ChatLTT <hdavy2002@gmail.com>
2024-09-05 16:41:34 +0530

    Staatic deployment aba3c34914aa

--- core/cache/asset-cleanup/js/item/content__uploads__custom-css-js__10386-js-v094ae2112f3f33fc31ee21cbcffb4dd0a92c74e1.js
+++
@@ -1,6 +0,0 @@
-(function(){function modifyLinks(){var links=document.getElementsByTagName('a');for(var i=0;i<links.length;i++){var link=links[i];link.removeAttribute('target');if(link.href.includes('/listing')){link.href=link.href.replace('https://chatltt.com','https://app.chatltt.com')}
-if(link.href.includes('static.chatltt.com/app.chatltt.com/submission')){link.href='https://app.chatltt.com/submission'}
-link.target='_self';link.addEventListener('click',function(event){this.target='_self'})}}
-if(document.readyState==="loading"){document.addEventListener("DOMContentLoaded",modifyLinks)}else{modifyLinks()}
-window.addEventListener('load',modifyLinks)})()
-;
